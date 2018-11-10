f52071604fd94939d8e86f2bd09f07f4f68b4f7a
Hussein El-Attar <hussien.elattar1@gmail.com>
2018-11-10 20:30:29 +0200

    Add function to filter empty strings out of an object

---
+++ filterEmptyStr.js
@@ -0,0 +1,30 @@
+function filterEmptyStr(Data) {
+    let filterdObj = {}
+    const keyValueArray = Object.entries(Data)
+    for (let i = 0; i < keyValueArray.length; i++) {
+        let key = keyValueArray[i][0]
+        let value = keyValueArray[i][1]
+
+        if (value.constructor === String) {
+            if (value !== "") {
+                filterdObj = { ...filterdObj, [key]: value }
+            }
+        }
+		else if (value.constructor === Number) {
+            if (value !== NaN) {
+                filterdObj = { ...filterdObj, [key]: value }
+            }
+        }
+        else if (value.constructor === Array) {
+            filterdObj = {
+                ...filterdObj, [key]: value.map(keyValueArray2 =>
+                    filterEmptyStr(keyValueArray2)).filter(keyValueArray3 =>
+                        Object.keys(keyValueArray3).length > 0)
+            }
+        }
+        else if (value.constructor === Object) {
+            filterdObj = { ...filterdObj, [key]: filterEmptyStr(value) }
+        }
+    }
+    return filterdObj
+}
